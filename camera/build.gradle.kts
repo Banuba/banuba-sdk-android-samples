7f09b58bdead56b246d332f810d94fae320c6fcc
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-04-25 15:30:31 +0300

    Template project

---
+++ camera/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    id("com.android.application").version("8.3.2")
+    id("org.jetbrains.kotlin.android").version("1.9.0")
+}
+
+android {
+    namespace = "com.banuba.sdk.example.camera"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.banuba.sdk.example.camera"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt")
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling:1.6.6")
+}
