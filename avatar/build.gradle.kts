9f5ae5a938d7047026de037b694b030d273950dd
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2025-03-27 18:19:25 +0300

    Fix project settings

--- avatar/build.gradle.kts
+++ avatar/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.banuba.sdk.example.avatar"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
